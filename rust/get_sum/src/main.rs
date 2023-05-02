e2874ac9135ee59172c7f027f6805b9d6a17a9e6
Egor Chunaev <ch.egor.yu@gmail.com>
2023-05-02 16:44:09 +0200

    add sum module

--- rust/get_sum/src/main.rs
+++ rust/get_sum/src/main.rs
@@ -1,3 +1,6 @@
+mod sum;
+use sum::get_sum;
+
 use std::io::{stdin, BufRead};
 
 fn main() {
@@ -13,7 +16,7 @@ fn main() {
 
         let parts = line_str.split(" ");
         for part in parts {
-            sum += part.parse::<i64>().expect("Failed to parse i64");
+            sum = get_sum(sum, part.parse::<i64>().expect("Failed to parse i64"));
             number_count += 1;
 
             if number_count >= 2 {
---
+++ rust/get_sum/src/sum.rs
@@ -0,0 +1,19 @@
+use std::ops::Add;
+
+pub fn get_sum<T: Add<Output = T>>(a: T, b: T) -> T {
+    return a + b;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_sum() {
+        assert_eq!(get_sum(0, 0), 0);
+        assert_eq!(get_sum(1, 2), 3);
+        assert_eq!(get_sum(-7, 11), 4);
+        assert_eq!(get_sum(-5, -1), -6);
+        assert_eq!(get_sum(234, 434), 668);
+    }
+}
