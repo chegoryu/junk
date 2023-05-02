082ca484351f79710915bba56ecb99ee705643f9
Egor Chunaev <ch.egor.yu@gmail.com>
2023-05-02 18:20:44 +0200

    fix codestyle

--- rust/get_sum/src/sum.rs
+++ rust/get_sum/src/sum.rs
@@ -14,6 +14,6 @@ mod tests {
         assert_eq!(get_sum(1, 2), 3);
         assert_eq!(get_sum(-7, 11), 4);
         assert_eq!(get_sum(-5, -1), -6);
-            assert_eq!(get_sum(234, 434), 668);
+        assert_eq!(get_sum(234, 434), 668);
     }
 }
