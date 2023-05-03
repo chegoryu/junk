52d92c47b0b457a0bf5536b95dcf0424e6a1c537
Egor Chunaev <ch.egor.yu@gmail.com>
2023-05-03 06:41:26 +0200

    add rust docker file to docker image workflow

--- docker/go_echo_server/build_dev.sh
+++ docker/go_echo_server/build_dev.sh
@@ -6,6 +6,6 @@ docker build \
     --rm \
     -t go-echo-server-dev:latest \
     -f $CODE_ROOT/docker/go_echo_server/Dockerfile \
-    --build-arg PKG=$(cat $CODE_ROOT/go/go.mod | head -n 1 | awk '{ print $2 }') \
+    --build-arg GO_PKG=$(cat $CODE_ROOT/go/go.mod | head -n 1 | awk '{ print $2 }') \
     --build-arg GIT_DESCRIBE=$(git describe --all --always --dirty --long) \
     $CODE_ROOT
