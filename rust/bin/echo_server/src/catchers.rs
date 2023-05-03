4dd00c1f1a430958fff9ff8ad6f7a93a376569e8
Egor Chunaev <ch.egor.yu@gmail.com>
2023-05-03 20:31:18 +0200

    add method to custom error catcher response

--- rust/bin/echo_server/src/catchers.rs
+++ rust/bin/echo_server/src/catchers.rs
@@ -3,7 +3,7 @@ use rocket::{catch, catchers, Build, Request, Rocket};
 
 #[catch(default)]
 fn default(status: Status, req: &Request) -> String {
-    format!("{} ({})\n", status, req.uri())
+    format!("{} ({} {})\n", status, req.method(), req.uri())
 }
 
 pub fn register_catchers(rocket_builder: Rocket<Build>) -> Rocket<Build> {
@@ -25,11 +25,22 @@ mod tests {
     fn test_default() {
         let client = get_client();
 
-        let response = client.get("/this_route_does_not_exist").dispatch();
-        assert_eq!(response.status(), Status::NotFound);
-        assert_eq!(
-            response.into_string(),
-            Some("404 Not Found (/this_route_does_not_exist)\n".to_owned())
-        );
+        {
+            let response = client.get("/this_route_does_not_exist").dispatch();
+            assert_eq!(response.status(), Status::NotFound);
+            assert_eq!(
+                response.into_string(),
+                Some("404 Not Found (GET /this_route_does_not_exist)\n".to_owned())
+            );
+        }
+
+        {
+            let response = client.post("/this_route_does_not_exist").dispatch();
+            assert_eq!(response.status(), Status::NotFound);
+            assert_eq!(
+                response.into_string(),
+                Some("404 Not Found (POST /this_route_does_not_exist)\n".to_owned())
+            );
+        }
     }
 }
