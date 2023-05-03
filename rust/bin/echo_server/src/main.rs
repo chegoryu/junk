868652775b43eea09a135bc58a9dd96ae46d8f2d
Egor Chunaev <ch.egor.yu@gmail.com>
2023-05-03 03:06:41 +0200

    add cli args to rust echo_server (for config); allow to customize config

--- rust/bin/echo_server/src/main.rs
+++ rust/bin/echo_server/src/main.rs
@@ -1,6 +1,16 @@
 #[macro_use]
 extern crate rocket;
 
+use clap::Parser;
+use figment::providers::{Format, Json};
+use rocket::config::Config;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value = "config.json")]
+    config: String,
+}
+
 #[get("/ping")]
 fn ping() -> &'static str {
     "pong"
@@ -8,7 +18,9 @@ fn ping() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    let rocket = rocket::build();
+    let args = Args::parse();
+    let config = Config::figment()
+        .merge(Json::file(args.config));
 
-    rocket.mount("/", routes![ping])
+    rocket::custom(config).mount("/", routes![ping])
 }
